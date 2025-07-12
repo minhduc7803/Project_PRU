0b590cf9bdaadcc0175769ca774024e2f1f1f25f
Mr.Warren <hiepdevo@gmail.com>
2025-07-12 21:47:31 +0700

    start

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+        public void OnStartButtonClicked()
+        {
+            SceneManager.LoadScene("Level 1"); // Đổi "MainScene" thành tên scene chính của bạn
+        }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
